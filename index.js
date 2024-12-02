800703d2ef56e7d5b4a4e40e35ef3a2a677aac39
Yuval Chen <118536986+yuvalchen1701@users.noreply.github.com>
2024-12-02 12:32:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ dest: 'uploads/' });
 
+app.post('/api/fileanalyse', upload.single('upfile'),
+(req, res) => {
+  const uploadedFile = req.file;
+
+  if(!uploadedFile) {
+    console.log('No file uploaded');
+  }
+
+  res.send({
+    name: uploadedFile.originalname,
+    type: uploadedFile.mimetype,
+    size: uploadedFile.size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
